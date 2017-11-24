033e89974fae0ec676034821bbbe1f4b70306dd0
Denis Gorodnichev <denis.gorodnichev@gmail.com>
2017-11-24 17:55:06 +0300

    snapshot operations

--- snapshot.py
+++ snapshot.py
@@ -15,7 +15,7 @@
 from __future__ import print_function
 
 import atexit
-
+import argparse
 import requests
 
 from pyVim.connect import SmartConnect, SmartConnectNoSSL, Disconnect
@@ -24,19 +24,42 @@ from pyVim.task import WaitForTask
 
 from tools import cli
 
+import pptree
+
 requests.packages.urllib3.disable_warnings()
 
+class Actions:
+    CREATE = 0
+    SWITCH = 1
+    REMOVE = 2
+    TREE = 3
 
 def setup_args():
     parser = cli.build_arg_parser()
     parser.add_argument('--stack-name', required=True,
                         help="stack name")
-    parser.add_argument('-d', '--description', required=False,
+    parser.add_argument('--create', required=False,
+                        dest="action", action="store_const", const=Actions.CREATE,
+                        help="Create snapshot")
+    parser.add_argument('--switch', required=False,
+                        dest="action", action="store_const", const=Actions.SWITCH,
+                        help="Switch to snapshot")
+    parser.add_argument('--remove', required=False,
+                        dest="action", action="store_const", const=Actions.REMOVE,
+                        help="Remove snapshot")
+    parser.add_argument('--tree', required=False,
+                        dest="action", action="store_const", const=Actions.TREE,
+                        help="Show snapshot tree")
+    parser.add_argument('--description', required=False,
                         help="Description for the snapshot")
-    parser.add_argument('-n', '--name', required=True,
+    parser.add_argument('--name', required=False,
                         help="Name for the Snapshot")
-    my_args = parser.parse_args()
-    return cli.prompt_for_password(my_args)
+    args = parser.parse_args()
+    if args.action is None:
+        parser.print_help()
+        exit(1)
+
+    return cli.prompt_for_password(args)
 
 def get_vms(si):
     content = si.RetrieveContent()
@@ -46,33 +69,74 @@ def get_vms(si):
     recursive = True  # whether we should look into it recursively
     containerView = content.viewManager.CreateContainerView(container, viewType, recursive)
 
-
     children = containerView.view
     for child in children:
         yield child
 
-args = setup_args()
-si = None
-instance_search = False
-try:
-    si = SmartConnectNoSSL(host=args.host,
-                               user=args.user,
-                               pwd=args.password)
-    atexit.register(Disconnect, si)
-except IOError:
-    pass
+def create_snapshot(si, stack_name, name, description):
+    vms = filter(lambda vm: vm.name.startswith(stack_name), get_vms(si))
+    desc = None
+    if description:
+        desc = description
 
-if not si:
-    raise SystemExit("Unable to connect to host with supplied info.")
+    tasks = map(lambda vm: vm.CreateSnapshot_Task(name=name, description=desc, memory=True, quiesce=False), vms)
+
+    for task in tasks:
+        WaitForTask(task)
+
+def list_snapshots(si, stack_name):
+    vms = filter(lambda vm: vm.name.startswith(stack_name), get_vms(si))
+    for vm in vms:
+        for rootSnapshot in vm.snapshot.rootSnapshotList:
+            pptree.print_tree(rootSnapshot, "childSnapshotList", "name")
+
+def _get_vm_snapshot_recursively(snapshots, snapshot_name):
+    for snapshot in snapshots:
+        if snapshot.name == snapshot_name:
+            return snapshot
+        else:
+            childSnapshot = _get_vm_snapshot_recursively(snapshot.childSnapshotList, snapshot_name)
+            if childSnapshot:
+                return childSnapshot
+
+    return None
+
+def switch_to_snapshot(si, stack_name, snapshot_name):
+    vms = filter(lambda vm: vm.name.startswith(stack_name), get_vms(si))
+    tasks = []
+    for vm in vms:
+        snapshotTree = _get_vm_snapshot_recursively(vm.snapshot.rootSnapshotList, snapshot_name)
+        if snapshotTree:
+            print ("Switching %s to %s" % (vm.name, snapshot_name))
+            tasks.append(snapshotTree.snapshot.RevertToSnapshot_Task())
+        else:
+            print ("%s have no snapshot %s" % (vm.name, snapshot_name))
+
+    for task in tasks:
+        WaitForTask(task)
 
 
-vms = filter(lambda vm: vm.name.startswith(args.stack_name), get_vms(si))
+if __name__ == "__main__":
+    args = setup_args()
 
-desc = None
-if args.description:
-    desc = args.description
+    si = None
+    instance_search = False
+    try:
+        si = SmartConnectNoSSL(host=args.host,
+                                   user=args.user,
+                                   pwd=args.password)
+        atexit.register(Disconnect, si)
+    except IOError:
+        pass
 
-tasks = map(lambda vm: vm.CreateSnapshot_Task(name=args.name, description=desc, memory=True, quiesce=False), vms)
+    if not si:
+        raise SystemExit("Unable to connect to host with supplied info.")
 
-for task in tasks:
-    WaitForTask(task)
+    if args.action == Actions.CREATE:
+        create_snapshot(si, args.stack_name, args.name, args.description)
+    elif args.action == Actions.TREE:
+        list_snapshots(si, args.stack_name)
+    elif args.action == Actions.SWITCH:
+        switch_to_snapshot(si, args.stack_name, args.name)
+    else:
+        exit(1)
